dfd949cf61b72d2694b4f03825af97a670f35f8a
Your Name <you@example.com>
2016-05-16 12:03:40 -0400

    Practice making a timer

---
+++ dayEightNestedLoopsTimerPractice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dayEightNestedLoopsTimerPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int hour = 12; hour >= 1; hour--)
+            {
+                for(int min = 60; min > 1; min--)
+                {
+                    for(int sec = 60; sec > 1; sec--)
+                    {
+                        Console.WriteLine(hour + ":" + min + ":" + sec);
+                        System.Threading.Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+    }
+}
